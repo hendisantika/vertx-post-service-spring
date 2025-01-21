cb365e20b15aa9e8423d6480666e313710cdd80d
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 08:25:54 +0700

    Add SpringAwareVerticleFactory🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_post_service_spring/SpringAwareVerticleFactory.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.vertx_post_service_spring;
+
+import io.vertx.core.spi.VerticleFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-post-service-spring
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 08.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class SpringAwareVerticleFactory implements VerticleFactory, ApplicationContextAware {
+
+  private ApplicationContext applicationContext;
+}
