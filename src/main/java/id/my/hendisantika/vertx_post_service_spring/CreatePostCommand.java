be03d27e47a521d98b06da84b7b6699d799e8540
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 08:18:37 +0700

    Add CreatePostCommand🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_post_service_spring/CreatePostCommand.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.vertx_post_service_spring;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-post-service-spring
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 08.18
+ * To change this template use File | Settings | File Templates.
+ */
+public record CreatePostCommand(String title, String content) {
+}
