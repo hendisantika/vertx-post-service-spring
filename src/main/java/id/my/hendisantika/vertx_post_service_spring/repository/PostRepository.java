2717f03e43dbb022d1ec6415b49c26b035386327
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 08:20:56 +0700

    Add PostRepository🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_post_service_spring/repository/PostRepository.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.vertx_post_service_spring.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-post-service-spring
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 08.20
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@RequiredArgsConstructor
+public class PostRepository {
+  private static final Logger LOGGER = Logger.getLogger(PostRepository.class.getName());
+
+}
